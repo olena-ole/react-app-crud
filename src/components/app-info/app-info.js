5e1efd2977ef416ed5cc31aebaf32a5369963b97
Lennna <pampam32121@gmail.com>
2023-02-06 01:49:10 +0200

    added components markup and styles

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>PMC Company Employees Data</h1>
+            <h2>Employees Total: </h2>
+            <h2>Receive Bonuses: </h2>
+        </div>
+    );
+};
+
+export default AppInfo;
