2e35a15bfd498385effe957a21a956084b837f4d
Lennna <pampam32121@gmail.com>
2023-02-06 03:04:35 +0200

    generated employees from dummy data

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,6 +6,13 @@ import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 function App() {
+
+    const data = [
+        {name: 'John C.', salary: 800, increase: true, id: 1},
+        {name: 'Alex W.', salary: 3000, increase: false, id: 2},
+        {name: 'Carl T.', salary: 5000, increase: true, id: 3}
+    ]
+
     return (
         <div className="app">
             <AppInfo />
@@ -15,7 +22,7 @@ function App() {
                 <AppFilter />
             </div>
 
-            <EmployeesList />
+            <EmployeesList data={data}/>
             <EmployeesAddForm />
         </div>
     );
